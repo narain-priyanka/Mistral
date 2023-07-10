36f4d7a85d80981f7442fab07ca3e6dd38c977b2
narain-priyanka <62747395+narain-priyanka@users.noreply.github.com>
2023-07-09 21:27:52 -0400

    Time: 55 ms (38.03%), Space: 16.3 MB (64.87%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
         memo=[0]*(n+2)
+        memo[1]=1
         if memo[n]:
             return memo[n]
-        memo[1]=1
         for i in range(2,n+1):
             memo[i]=memo[i-1]+memo[i-2]
         return memo[n]
