a0081c93659543a0f9bbca06dc4bc71cc8a2503e
narain-priyanka <62747395+narain-priyanka@users.noreply.github.com>
2023-07-27 23:58:15 -0400

    Time: 2 ms (94.7%), Space: 41.1 MB (18.95%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,17 +1,32 @@
 import java.util.Collections;
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        // Store the frequency of elements in the unordered map.
-        Map<Integer, Integer> freq = new HashMap<>();
-        for (int num : arr) {
-            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        Map<Integer,Integer> map = new HashMap<>();
+        for(Integer each: arr){
+            if(map.get(each)==null){
+                map.put(each,1);
+            }
+            else{
+               int val= map.get(each);
+               map.put(each,val+1);
+            }
         }
+        Set<Integer> s= new HashSet<>(map.values());
+        return map.size()==s.size();
+
         
-        // Store the frequency count of elements in the unordered set.
-        Set<Integer> freqSet = new HashSet<>(freq.values());
-        
-        // If the set size is equal to the map size, 
-        // It implies frequency counts are unique.
-        return freq.size() == freqSet.size();
     }
 }
+
+// // Store the frequency of elements in the unordered map.
+//         Map<Integer, Integer> freq = new HashMap<>();
+//         for (int num : arr) {
+//             freq.put(num, freq.getOrDefault(num, 0) + 1);
+//         }
+        
+//         // Store the frequency count of elements in the unordered set.
+//         Set<Integer> freqSet = new HashSet<>(freq.values());
+        
+//         // If the set size is equal to the map size, 
+//         // It implies frequency counts are unique.
+//         return freq.size() == freqSet.size();
