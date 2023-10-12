531729f16555356beaa3d7e978c1afe140c0e231
narain-priyanka <62747395+narain-priyanka@users.noreply.github.com>
2023-10-13 02:48:41 +0530

    Time: 0 ms (100%), Space: 41.3 MB (41.53%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,58 +8,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-//         ListNode dummynode = new ListNode(0);
-//         ListNode current = dummynode;
-
-//         while(list1 != null && list2 != null){
-//             if(list1.val < list2.val){
-//                 current.next = new ListNode(list1.val);
-//                 current= current.next;
-//                 list1=list1.next;
-//             }
-//             else{
-//                 current.next = new ListNode(list2.val);
-//                 current= current.next;
-//                 list2=list2.next;
-//             } 
-
-//             if(list1 !=null){
-//                 current.next=list1;
-//             }
-//             else{
-//                 current.next=list2;
-//             }
-//         }
-//         return dummynode.next;
-//     }
-// }
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummyNode = new ListNode(0);
-        ListNode current = dummyNode;
+        ListNode dummynode = new ListNode(0);
+        ListNode current = dummynode;
 
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
                 current.next = new ListNode(list1.val);
-                current = current.next;
-                list1 = list1.next;
-            } else {
+                current= current.next;
+                list1=list1.next;
+            }
+            else{
                 current.next = new ListNode(list2.val);
-                current = current.next;
-                list2 = list2.next;
+                current= current.next;
+                list2=list2.next;
             }
         }
 
-        // Append the remaining nodes (if any) from list1 or list2
-        if (list1 != null) {
-            current.next = list1;
-        } else {
-            current.next = list2;
+        if(list1 !=null){
+            current.next=list1;
         }
-
-        return dummyNode.next;
+        else{
+            current.next=list2;
+        }
+        return dummynode.next;
     }
 }
